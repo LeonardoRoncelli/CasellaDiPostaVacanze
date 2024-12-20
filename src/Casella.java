e8a734aa23172e9e865e6e3fa1b73e22966ce0ad
Roncelli <leonardoroncelli@gmail.com>
2024-12-20 18:43:04 +0100

    Attributi e metodi set e get della classe Email.

---
+++ src/Casella.java
@@ -0,0 +1,3 @@
+public class Casella extends Email{
+
+}
--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,16 @@
 public class Email {
-
+    private String testo;
+    private String mittente;
+    public void setTesto(String testo){
+        this.testo=testo;
+    }
+    public String getTesto(){
+        return testo;
+    }
+    public void setMittente(String mittente){
+        this.mittente=mittente;
+    }
+    public String getMittente(){
+        return mittente;
+    }
 }
